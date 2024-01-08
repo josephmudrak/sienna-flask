a5664fc76fefcf151afec3a17970819be7ca50f1
Joseph Mudrak <josephedmudrak@gmail.com>
2024-01-08 18:30:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Sienna – Your Dynamic AI Voice Companion
+
+This repository contains the source code for Sienna. It can run locally on the
+user’s machine.
+
+## Requirements
+Sienna requires the following to run properly:
+- An audio input and output device
+- A web browser and Internet connection
+	- So far, Sienna has only been tested in Firefox, but it should be able to
+	run in other browsers.
+- Python ≥ 3.10
+- The following Python packages: `Flask Flask_Cors Flask_SocketIO openai python-dotenv websockets`
+	- You may need to install additional packages depending on your operating
+	environment.
+
+*Note:* Sienna has been confirmed to work on Ubuntu Desktop and Fedora
+Workstation. It **may or may not** work on other systems, so please report any
+additional testing.
+
+## Usage
+To run Sienna, type `python3 index.py` to start the server. Then, navigate to
+http://localhost:3000 in your browser. The Sienna UI should be displayed.
+
+### API Keys
+To run Sienna yourself, you will need an API key from OpenAI and ElevenLabs.
+**These are not free of charge**.
+
+Once you have your keys, please set them as environment variables like this:
+```
+ELEVENLABS_API_KEY=<ElevenLabs API key>
+OPENAI_API_KEY=<OpenAI API key>
+```
+
+Alternatively, you can put the above into a file named `.env`, and save this
+file in the same directory as the Python script.
---
+++ static/audio.js
@@ -0,0 +1,176 @@
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const btnStart	= document.getElementById("recStart");
+
+	navigator.mediaDevices.getUserMedia({audio: true}).then(stream =>
+	{
+		const mediaRecorder	= new MediaRecorder(stream);
+		
+		btnStart.addEventListener("click", () =>
+		{
+			// Initialise connection to Deepgram
+			const dgSocket	= new WebSocket(
+				"wss://api.deepgram.com/v1/listen",
+				["token", "a12a55ee5b9e4bb5ed14bcbf6f8af42acfa0c63d"]
+			);
+
+			// Logging WebSocket events
+			dgSocket.onopen	= () =>
+			{
+				// Send data to Deepgram in increments
+				mediaRecorder.addEventListener("dataavailable", event =>
+				{
+					if (event.data.size > 0 && dgSocket.readyState == 1)
+					{
+						dgSocket.send(event.data);
+					}
+				});
+
+				mediaRecorder.start(150);
+			}
+
+			dgSocket.onmessage	= async(msg) =>
+			{
+				// Parse data sent from Deepgram
+				const dgReceived	= JSON.parse(msg.data);
+				const dgTranscript	=
+					dgReceived.channel.alternatives[0].transcript;
+				
+				if (dgTranscript && dgReceived.is_final)
+				{
+					console.log(dgTranscript);
+					mediaRecorder.stop();
+					dgSocket.close();
+
+					// Send to GPT-4 & ElevenLabs
+					await fetch("/reply", {method: "POST", body: dgTranscript});
+					
+					// WebSocket compatible with SocketIO to back-end
+					const elSocket	= io.connect(
+						"http://localhost:3000/stream"
+					);
+
+					// Queue for audio chunks
+					let audioChunks		= [];
+
+					// Audio currently playing?
+					let isPlaying		= false;
+
+					// Logging
+					elSocket.on("connect", () =>
+					{
+						console.log("WebSocket connection opened")
+					})
+
+					elSocket.on("audio_chunk", (data) =>
+					{
+						// Add audio chunk to queue
+						audioChunks.push(data.audio);
+
+						// If not currently playing, start playback
+						if (!isPlaying)
+						{
+							playNextChunk();
+						}
+					});
+
+					elSocket.on("disconnect", () =>
+					{
+						console.log("WebSocket connection closed")
+					});
+
+					async function playNextChunk()
+					{
+						// Chunks remaining in queue?
+						if (audioChunks.length > 0)
+						{
+							isPlaying	= true;
+
+							const audioContext	= new AudioContext();
+
+							// Convert base64 to ArrayBuffer
+							const audioArrBuf	= base64ToArrayBuffer(
+								audioChunks.shift()
+							);
+
+							// Decode ArrayBuffer to AudioBuffer
+							const audioBuf	= 
+								await audioContext.decodeAudioData(
+									audioArrBuf
+							);
+							
+							const src	= audioContext.createBufferSource();
+							src.buffer	= audioBuf;
+							src.connect(audioContext.destination);
+
+							// Set callback for playback end
+							src.onended	= () =>
+							{
+								isPlaying	= false;
+
+								// Close AudioContext after playback
+								audioContext.close();
+
+								playNextChunk();
+							}
+
+							src.start();
+						}
+					}
+				}
+			}
+
+			dgSocket.onerror	= (err) =>
+			{
+				console.log({event: "onerror", err});
+			}
+		});
+	})
+	.catch(err => console.error("Error accessing microphone:", err));
+});
+
+// Convert base64-encoded string to Blob
+function base64toBlob(base64, type)
+{
+	const strBin	= window.atob(base64);
+	const len		= strBin.length;
+	const bytes		= new Uint8Array(len);
+	
+	for (let i = 0; i < len; i++)
+	{
+		bytes[i]	= strBin.charCodeAt(i);
+	}
+
+	return new Blob([bytes], {type: type});
+}
+
+// Convert Blob to ArrayBuffer
+function audioBlobToArrayBuffer(blob)
+{
+	return new Promise((resolve) =>
+	{
+		const reader	= new FileReader();
+
+		reader.onloadend	= () =>
+		{
+			resolve(reader.result);
+		}
+
+		reader.readAsArrayBuffer(blob);
+	});
+}
+
+// Convert base64 to ArrayBuffer
+function base64ToArrayBuffer(base64)
+{
+	const strBin	= window.atob(base64);
+	const len		= strBin.length;
+	const bytes		= new Uint8Array(len);
+
+	for (let i = 0; i < len; i++)
+	{
+		bytes[i]	= strBin.charCodeAt(i);
+	}
+
+	return bytes.buffer;
+}
